eecee7bba81767c128119a29d7789630aebcdf1e
Admin <183120084@qq.com>
2017-04-03 09:55:23 +0800

    登录之后就执行 一次

--- src/main/java/com/melot/executor/UserResidualExecutor.java
+++ src/main/java/com/melot/executor/UserResidualExecutor.java
@@ -18,21 +18,28 @@ public class UserResidualExecutor implements Executor {
         schedule.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
-                Map<Integer, String> userMap = HandleExecutor.getUserMap();
-                if (!userMap.isEmpty()) {
-                    Iterator<Integer> it = userMap.keySet().iterator();
-                    int totalAmount = 0;
-                    while (it.hasNext()) {
-                        int userId = it.next();
-                        String token = userMap.get(userId);
-                        int amount = Operater.getUserInfo(userId, token);
-                        System.out.println(userId + "has money(rmb): " + amount / 1000);
-                        totalAmount += amount;
+                try {
+                    Map<Integer, String> userMap = HandleExecutor.getUserMap();
+                    if (!userMap.isEmpty()) {
+                        Iterator<Integer> it = userMap.keySet().iterator();
+                        int totalAmount = 0;
+                        while (it.hasNext()) {
+                            int userId = it.next();
+                            String token = userMap.get(userId);
+                            int amount = Operater.getUserInfo(userId, token);
+                            System.out.println(userId + " has money(rmb): " + amount / 1000);
+                            totalAmount += amount;
+                            Thread.sleep(5000L);
+                        }
+                        System.out.println("total money " + totalAmount / 1000);
                     }
-                    System.out.println("total money " + totalAmount / 1000);
+                } catch (Exception e) {
+                    System.out.println(" UserResidualExecutor error !");
+                    e.printStackTrace();
                 }
+
             }
-        }, 1, 24 * 60, TimeUnit.MINUTES);
+        }, 10, 24 * 60 * 60, TimeUnit.SECONDS);
     }
 
 
