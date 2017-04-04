aeb7b588519d86a53ad9826deab913ffa227ad6f
Admin <183120084@qq.com>
2017-04-04 12:21:00 +0800

    换成单线程 抢红包看看

---
+++ src/main/java/com/red/client/SimpleSocketClientSlave.java
@@ -0,0 +1,69 @@
+package com.red.client;
+
+import com.google.gson.JsonObject;
+import com.red.util.DataUtil;
+import com.red.util.OperUtil;
+import com.red.util.PoolUtil;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Set;
+
+@ClientEndpoint
+public class SimpleSocketClientSlave {
+
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("slave open....");
+    }
+
+    @OnMessage
+    public void onMessage(String message) throws Exception {
+        if (message.contains("sendId")) {
+            JsonObject json = (JsonObject) DataUtil.JSON_PARSER.parse(message);
+            final String sendId = json.get("sendId").getAsString();
+            final String roomId = json.get("roomId").getAsString();
+
+            if (DataUtil.RED_FILTER.putIfAbsent(roomId + "_" + sendId, System.currentTimeMillis() + 2 * 60000) != null) {
+                return;
+            }
+            Set<String> userIdSet = DataUtil.UESER_TOKEN.keySet();
+            for (final String userId : userIdSet) {
+                final String token = DataUtil.UESER_TOKEN.get(userId);
+                try {
+                    OperUtil.getRed(userId, token, sendId, roomId);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @OnClose
+    public void onClose() throws IOException {
+        System.out.println("slave close...");
+    }
+
+    @OnError
+    public void onError(Throwable t) {
+        t.printStackTrace();
+    }
+
+    public static void connect(String userId, String roomId, String token, String ws) throws Exception {
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        Session session = container.connectToServer(SimpleSocketClientSlave.class, URI.create(ws));
+        JsonObject msg = new JsonObject();
+        msg.addProperty("MsgTag", 10010201);
+        msg.addProperty("platform", 1);
+        msg.addProperty("roomId", roomId);
+        msg.addProperty("container", 1);
+        msg.addProperty("softVersion", 10040);
+        msg.addProperty("linking", "www.kktv5.com");
+        msg.addProperty("userId", userId);
+        msg.addProperty("token", token);
+        session.getBasicRemote().sendText(msg.toString());
+        DataUtil.putSession(userId, roomId, System.currentTimeMillis() + 3 * 60 * 1000, session);
+    }
+
+}
